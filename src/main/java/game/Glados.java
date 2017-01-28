37c49daf4b0e5fc26cd28234b3dfe54def674ca0
E-stein <eloise.stein@outlook.fr>
2017-01-28 23:07:35 +0100

    Ajout IA

---
+++ src/main/java/game/Glados.java
@@ -0,0 +1,9 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Public class which describe IA
+public class Glados extends Player {
+  protected String name = "Glados";
+  protected String baseDeDonnees = "file/dico.txt";
+}
